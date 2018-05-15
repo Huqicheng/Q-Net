fix bugs of expoention overflow and add the new example of titanc dataset

deeplearning/func.py
@@ -1,18 +1,48 @@
 from deeplearning.tensor import Tensor
 import numpy as np
+from scipy.special import expit
 
 
-def softmax(x: Tensor) -> Tensor:
-    exps = np.exp(x.T)
-    sum = exps.sum(axis=0)
-    res = exps / sum
-    res = res.T
-    return res
+def softmax(X: Tensor,
+            theta: float = 1.0,
+            axis: int = 1 ) -> Tensor:
+    """
+        reference:
+        https://nolanbconaway.github.io/blog/2017/softmax-numpy
+        
+        Compute the softmax of each element along an axis of X.
+        
+        Parameters
+        ----------
+        X: ND-Array. Probably should be floats.
+        theta (optional): float parameter, used as a multiplier
+        prior to exponentiation. Default = 1.0
+        axis (optional): axis to compute values along. Default is the
+        first non-singleton axis.
+        
+        Returns an array the same size as X. The result will sum to 1
+        along the specified axis.
+    """
+    y = X
+    # multiply y against the theta parameter
+    y = y * float(theta)
+    # subtract the max for numerical stability
+    y = y - np.expand_dims(np.max(y, axis = axis), axis)
+    # exponentiate y
+    y = np.exp(y)
+    # take the sum along the specified axis
+    ax_sum = np.expand_dims(np.sum(y, axis = axis), axis)
+    # finally: divide elementwise
+    p = y / (ax_sum + np.finfo(float).eps)
+
+    return p
+
 
 
 def softmax_derivative(x: Tensor) -> Tensor:
     """
         derivative of softmax function
+        
         https://www.dropbox.com/s/rxrtz3auu845fuy/Softmax.pdf?dl=0
     """
     output = softmax(x)
@@ -27,8 +57,9 @@ def tanh_derivative(x: Tensor) -> Tensor:
 
 
 
+
 def sigmoid(x: Tensor) -> Tensor:
-    output = 1/(1+np.exp(-x))
+    output = expit(x)
     return output
 
 def sigmoid_derivative(x: Tensor) -> Tensor: